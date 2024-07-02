2f472c6ae23642fe77dbf1136c819aa8f374b288
rakibhossain01 <rakibhossain157741@gmail.com>
2024-07-02 22:22:04 +0600

    Added

--- src/pacman/Model.java
+++ src/pacman/Model.java
@@ -16,8 +16,8 @@ public class Model extends JPanel implements ActionListener {
     private boolean inGame = false;
     private boolean dying = false;
 
-    private final int BLOCK_SIZE = 24;
-    private final int N_BLOCKS = 15;
+    private final int BLOCK_SIZE = 24;//24
+    private final int N_BLOCKS = 15;//15
     private final int SCREEN_SIZE = N_BLOCKS * BLOCK_SIZE;
     private final int MAX_GHOSTS = 12;
     private final int PACMAN_SPEED = 6;
